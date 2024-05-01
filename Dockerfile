ef5d3c601ad97012552fda8f3b354ff156a10ee2
fferdinandpp <ferditetsuya22@gmail.com>
2024-05-02 05:24:07 +0700

    env

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM golang:1.22.0-alpine3.19
 
+RUN apk update && apk --no-cache add bash curl
+
+ENV HELLO_MESSAGE="Hello Universe!"
+ENV PORT=8080
+
 COPY main.go /app/main-simple.go
 
 CMD [ "go", "run", "/app/main-simple.go" ]
--- main-simple.go
+++ main-simple.go
@@ -3,14 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Ferdinand Pratama Putra")
+	message := os.Getenv("HELLO_MESSAGE")
+	if message == "" {
+		message = "Hello World! (no set env)"
+	}
+	fmt.Fprintf(w, message)
 }
 
 func main() {
 	http.HandleFunc("/", helloWorld)
-	fmt.Println("Server running at http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Printf("Server running at http://localhost:%s\n", port)
+	err := http.ListenAndServe(":" + port, nil)
+	if err != nil {
+		fmt.Printf("Failed to start server: %v\n", err)
+	}
 }
